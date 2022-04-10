364439d1156d3ecd0f381c9ae50f252a2e5e5db9
realludaga <ldh19971027@gmail.com>
2022-04-11 03:40:53 +0900

    complete

--- assets/js/part5.js
+++ assets/js/part5.js
@@ -13,17 +13,15 @@ const test = () => {
         msg.innerHTML = "클릭!!";
         msg2.innerHTML = "화면을 클릭해주세요!"
     }, Math.floor(Math.random() * 3000));
+    
     document.getElementById("test").addEventListener('click', () => {
-            clearTimeout(timer);
+      clearTimeout(timer);
             document.querySelector("#test").style.backgroundColor = "#F6BB43";
             end = new Date();
             msg.innerHTML = (end - start) + " ms";
             msg2.innerHTML = "";
             setTimeout(() => {
-                document.querySelector("#test").style.backgroundColor = "#3FE87F";
-                msg.innerHTML = "준비";
-                msg2.innerHTML = "준비가 완료되었으면 클릭해주세요!"
-                reload();
+                location.reload();
             }, 2000);
         })
 }
